15f8d89a5aeb0262914b614680049e3b23ce6876
Matheus Almeida <1519matheus@gmail.com>
2023-06-28 17:03:00 -0300

    Angular Address Finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,19 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { catchError, map } from 'rxjs';
+
+interface AddressFromApi {
+  cep: string
+  logradouro: string 
+  complemento: string
+  bairro: string
+  localidade: string
+  uf: string
+  ibge: string
+  gia: string
+  ddd: string
+  siafi: string
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +21,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-address';
+  cep = ''
+  street = ''
+  number = ''
+  complement = ''
+  state = ''
+  city = ''
+  neighborhood = ''
+
+  constructor(private http: HttpClient){}
+
+  onInputCep(){
+    if(this.cep.length === 8){
+     this.http.get<AddressFromApi>(`https://viacep.com.br/ws/${this.cep}/json/`)
+        .subscribe(data => {
+          this.street = data.logradouro
+          this.state = data.uf
+          this.city = data.localidade
+          this.neighborhood = data.bairro
+        })
+    }
+  }
 }
